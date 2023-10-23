1d2c5cdca8b13cc85e3d1c22d34948ca12c785b7
okkartal <okadirkartal@gmail.com>
2023-10-24 00:06:58 +0300

    added solution file  and shared project

---
+++ Shared/Entities/PolygonResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Entities;
+public class PolygonResponse
+{
+    [JsonPropertyName("results")]
+    public News[] Results { get; set; }
+
+    [JsonPropertyName("status")]
+    public string Status { get; set; }
+
+    [JsonPropertyName("request_id")]
+    public string RequestId { get; set; }
+
+    [JsonPropertyName("count")]
+    public long Count { get; set; }
+
+    [JsonPropertyName("next_url")]
+    public Uri NextUrl { get; set; }
+}
+
---
+++ Shared/Entities/Publisher.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Entities;
+public class Publisher
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("homepage_url")]
+    public Uri HomepageUrl { get; set; }
+
+    [JsonPropertyName("logo_url")]
+    public Uri LogoUrl { get; set; }
+
+    [JsonPropertyName("favicon_url")]
+    public Uri FaviconUrl { get; set; }
+}
